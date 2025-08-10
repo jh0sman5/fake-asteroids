38acb89f7afe5e54f3f598d1711ca0c9ea82155b
jhosman05 <justin@sahlegal.com>
2025-08-10 07:59:32 -0600

    we can now see the player triangle on the screen... yay

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import pygame
 
-
-
 from constants import *
+
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2), PLAYER_RADIUS)
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
+        
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
